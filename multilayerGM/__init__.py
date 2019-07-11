fa2d47b7fdb041940e8a53a2b1089adedd6b6310
Lucas Jeub <lucasjeub@gmail.com>
2019-07-11 10:42:24 +0200

    Use `setuptools_scm` to automatically set version number

--- multilayerGM/__init__.py
+++ multilayerGM/__init__.py
@@ -2,3 +2,11 @@ from . import dependency_tensors
 from . import export
 from .networks import multilayer_DCSBM_network
 from .partitions import sample_partition, dirichlet_null
+from pkg_resources import get_distribution, DistributionNotFound
+
+
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    # package is not installed
+    pass
